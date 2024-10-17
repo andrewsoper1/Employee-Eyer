9e5e518b46b62fa44da19ff0c93d67ffbebd322d
milkymilk <fluffypuffmalloween@gmail.com>
2024-10-16 18:12:14 -0600

    added gitignore,.env, package.json, and schema and seeds files

---
+++ Code/db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (department_name)
+VALUES ('Mergers and Acquisitions'),
+       ('Corporate Finance'),
+       ('Sales and Trading');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Vice President', 200000, 1),
+       ('Assistant to the Regional Manager', 100000, 2),
+       ('Señor Analyst', 60000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Patrick', 'Bateman', 111, NULL),
+       ('Paul', 'Allen', 222, 747),
+       ('Timothy', 'Bryce', 333, )
+    
+
+    
+       
+
+    
