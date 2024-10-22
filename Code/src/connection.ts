6856708028718b48b40b087054a9ed7ed0070813
milkymilk <fluffypuffmalloween@gmail.com>
2024-10-22 15:09:08 -0600

    updated package.json file, added tsconfig file, created src folder with files for roles, departments, and employees queries. created connection.ts file

---
+++ Code/src/connection.ts
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import pg from 'pg';
+
+const { Pool } = pg;
+
+const pool = new Pool({
+    
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: 'localhost',
+    database: process.env.DB_NAME,
+    port: 5432
+})
+
+export const connectDB = async  () => {
+    await pool.connect()
+}
+
+export const disconnectDB  = async () => {
+    await pool.end();
+}
+
+export const queryDB = async (text: string, params?: any[]) => {
+    const res = await pool.query(text, params);
+    return res.rows;
+}
+
+
